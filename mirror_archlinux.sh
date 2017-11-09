281cda8e267cc9c444d08ec17c5df37b598ef8a4
Francis Levasseur <levasseur.fr@gmail.com>
2017-11-09 14:35:49 -0500

    Update mirror_archlinux.sh

--- mirror_archlinux.sh
+++ mirror_archlinux.sh
@@ -2,17 +2,17 @@
 
 cd /archlinux_mirror
 
-RSYNC=/usr/bin/rsync
+#RSYNC=/usr/bin/rsync
 
-MIRROR=rsync://mirror.sergal.org/archlinux/
-MIRROR=rsync://mirror.its.dal.ca/archlinux/
-MIRROR=rsync://mirror.csclub.uwaterloo.ca/archlinux/
-MIRROR=rsync://muug.ca/mirror/archlinux/
+#MIRROR=rsync://mirror.sergal.org/archlinux/
+#MIRROR=rsync://mirror.its.dal.ca/archlinux/
+#MIRROR=rsync://mirror.csclub.uwaterloo.ca/archlinux/
+#MIRROR=rsync://muug.ca/mirror/archlinux/
 #MIRROR=rsync://mirror.rit.edu/archlinux/
 
-OPTIONS=-"rtlvH --delete-after --delay-updates --safe-links --max-delete=3000"
+#OPTIONS=-"rtlvH --delete-after --delay-updates --safe-links --max-delete=3000"
 
-$RSYNC $OPTIONS $MIRROR ./ > /dev/null
+#$RSYNC $OPTIONS $MIRROR ./ > /dev/null
+rsync -rtlH --delete-after --delay-updates --safe-links --max-delete=3000 rsync://mirror.csclub.uwaterloo.ca:/archlinux/ archlinux/ > /dev/null
 
 date > __last_sync
-

1427016924aa64df3a5d17a894ca5a46f5de0c75
Francis Levasseur <levasseur.fr@gmail.com>
2017-11-09 11:27:34 -0500

    Update mirror_archlinux.sh
    set list of canadian servers

--- mirror_archlinux.sh
+++ mirror_archlinux.sh
@@ -4,10 +4,10 @@ cd /archlinux_mirror
 
 RSYNC=/usr/bin/rsync
 
-#MIRROR=rsync://mirrors.uk2.net/archlinux/
-MIRROR=rsync://mirror.de.leaseweb.net/archlinux/
-#MIRROR=rsync://ftp5.gwdg.de/pub/linux/archlinux/
-#MIRROR=rsync://rsync.gtlib.gatech.edu/archlinux/
+MIRROR=rsync://mirror.sergal.org/archlinux/
+MIRROR=rsync://mirror.its.dal.ca/archlinux/
+MIRROR=rsync://mirror.csclub.uwaterloo.ca/archlinux/
+MIRROR=rsync://muug.ca/mirror/archlinux/
 #MIRROR=rsync://mirror.rit.edu/archlinux/
 
 OPTIONS=-"rtlvH --delete-after --delay-updates --safe-links --max-delete=3000"
